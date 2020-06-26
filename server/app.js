44c283f00af1c12d1feed79721969cd214d3ebb0
airell <joeyjordanwilliams@gmail.com>
2020-06-26 21:36:36 +0700

    otw socket io

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 3001;
+const http = require("http").Server(app);
 const routes = require("./routes");
+
 const errorHandler = require("./middlewares/errorHandler");
-
-
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,6 +18,6 @@ app.use(routes);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(PORT, "========================================");
 });
