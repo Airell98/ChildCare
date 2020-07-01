677c9a1a5c693596fc5694f6b038c7ffa2bd5245
fadhlanfm <fafadhlanriz@gmail.com>
2020-07-01 15:18:27 +0700

    image api udah bisa dipakai

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -22,12 +22,19 @@
       <b-form-select v-if="user == 'parent'" v-model="gender" :options="genders" />
       <b-form-input type="text" name="Address" placeholder="Address" v-model="address" />
       <b-form-input type="text" name="City" placeholder="City" v-model="city" />
-      <b-form-input
+      <!-- <b-form-input
         v-if="user == 'agency'"
         type="text"
         name="URL logo"
         placeholder="URL logo"
         v-model="logoUrl"
+      />-->
+      <b-form-file
+        v-if="user == 'agency'"
+        @change.prevent="uploadImg"
+        multiple
+        accept="image/jpeg, image/png"
+        ref="myFiles"
       />
       <b-form-input
         type="text"
@@ -44,6 +51,7 @@
 <script>
 import axios from "axios";
 import Swal from "sweetalert2";
+import resizeImage from "resize-image";
 
 export default {
   name: "Register",
@@ -105,6 +113,84 @@ export default {
           data: userData
         });
       }
+    },
+    previewFiles(event) {
+      console.log(event.target.files);
+    },
+    uploadImg(event) {
+      let imageUrl;
+      // var $files = $("input[type=file]").get(0).files;
+      var files = event.target.files[0];
+      console.log(files, "files");
+      // var img = new Image();
+      // img.src = url; // local image url
+
+      if (files.length) {
+        // Reject big files
+        // if (files[0].size > $(this).data("max-size") * 1024) {
+        //   console.log("Please select a smaller file");
+        //   return false;
+        // }
+
+        // Begin file upload
+        console.log("Uploading file to Imgur..");
+
+        // Replace ctrlq with your own API key
+        var apiUrl = "https://api.imgur.com/3/image";
+        var apiKey = "6b9f0e258a0d16b";
+
+        // var settings = {
+        //   async: false,
+        //   crossDomain: true,
+        //   processData: false,
+        //   contentType: false,
+        //   type: "POST",
+        //   url: apiUrl,
+        //   headers: {
+        //     Authorization: "Client-ID " + apiKey,
+        //     Accept: "application/json"
+        //   },
+        //   mimeType: "multipart/form-data"
+        // };
+
+        var formData = new FormData();
+        formData.append("image", files[0]);
+        // settings.data = formData;
+
+        // Response contains stringified JSON
+        // Image URL available at response.data.link
+        axios({
+          async: false,
+          crossDomain: true,
+          processData: false,
+          contentType: false,
+          method: "POST",
+          url: apiUrl,
+          headers: {
+            Authorization: "Client-ID " + apiKey,
+            Accept: "application/json"
+          },
+          mimeType: "multipart/form-data",
+          data: formData
+        })
+          .then(response => {
+            // console.log(response.data.data.link, "response");
+            this.logoUrl = response.data.data.link;
+            console.log(this.logoUrl, "sdfh");
+          })
+          .catch(err => {
+            console.log(err.response.data);
+            this.logoUrl =
+              "https://e7.pngegg.com/pngimages/379/878/png-clipart-nanny-childcare-worker-child-care-infant-child-nanny-childcare.png";
+            Swal.fire({
+              icon: "error",
+              title: "Upload image failed",
+              text: "Changed to default image"
+            });
+          });
+      }
+      // $("input[type=file]").val("");
+      return imageUrl;
     }
     // onSubmitDataRegister() {
     //   this.onConfirmDataRegister({
